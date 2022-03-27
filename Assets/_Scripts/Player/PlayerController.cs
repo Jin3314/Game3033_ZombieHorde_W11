cd30185808d4427de8c3ace3be9fe0d54879356e
Jin3314 <dudwlstldud@gmail.com>
2022-03-27 18:43:43 -0400

    Week11 Submission
    Week11 Submission

---
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -0,0 +1,37 @@
+///----------------------------------------------------------------------------------
+///   Endless Horde Zombie Shooter
+///   PlayerController.cs
+///   Author            : Vineet Kumar
+///   Last Modified     : 2022/01/19
+///   Description       : Player Controller bools for checking movement and action
+///   Revision History  : 1st ed. Set jump, run, fire and reload bools to refer
+///----------------------------------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Movement
+    public bool isJumping;
+    public bool isRunning;
+
+    // Combat
+    public bool isFiring;
+    public bool isReloading;
+    public bool isAiming;
+
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    
+    //}
+    //
+    //// Update is called once per frame
+    //void Update()
+    //{
+    //    
+    //}
+}
